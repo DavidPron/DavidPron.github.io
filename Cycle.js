ad710ee8a44127ce3dd6e7c7d049a178faf8fdf1
DavidPron <92371541+DavidPron@users.noreply.github.com>
2023-08-24 11:23:57 +0300

    Create Cycle.js
    One of the first attempts to use JS

---
+++ Cycle.js
@@ -0,0 +1,10 @@
+// Create a variable to store the current number
+var i = 1;
+
+// Cycle while
+while (i <= 10) {
+  // Print the sentence
+  console.log("№" + i);
+  // Increase the current number by 1
+  i++;
+}
